93dfc6e5998974fba28b848f94d06ace9de8716c
Steve DeAnda <stevedeanda5@gmail.com>
2022-06-10 21:00:33 -0500

    Add moved things around

--- projects/learning-php/footer.php
+++ projects/learning-php/footer.php
@@ -5,4 +5,4 @@
 			
 		<p>I just wanted to highlight the first things I learned in PHP. I learned to put strings together, how to do math, practice the syntax, how to reference data, and how to print using echo. In the end, PHP is a scripting language that generates HTML.</p>
 
-	</footer>
+</footer>
