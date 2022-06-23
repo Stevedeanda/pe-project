f6925793b6cccd10a27c0bd26ca639ba1d918524
Steve DeAnda <stevedeanda5@gmail.com>
2022-06-23 15:26:18 -0500

    Add updates to meta-theme challenge

--- projects/theme-challenge/index.php
+++ projects/theme-challenge/index.php
@@ -6,9 +6,9 @@
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-		<title>PSSST (CSS)</title>
-		<meta name='description' content='PSSST (CSS)'>
-		<meta property='og:image' content='A CSS methodology so great, it needed a name.'>
+		<title>Five Nine</title>
+		<meta name='description' content='Outdoor apparel with the environment in mind'>
+		<meta property='og:image' content='images/fivenine-meta.jpg'>
 
 		<link rel='stylesheet' href='styles/site.css'>
 	</head>
