8ee22590e85fbbac27641d3c846f589a9bc35fff
Steve DeAnda <stevedeanda5@gmail.com>
2022-07-05 13:50:06 -0500

    Add changes to form

--- projects/php-crud/modules/form/create.php
+++ projects/php-crud/modules/form/create.php
@@ -1,7 +1,92 @@
 <?php
 
-	if (isset($_POST["add"])) {
-		echo "added";
+	$song = null;
+	$album = null;
+	$artist = null;
+	$genre = "";
+	$year = 0;
+	$picture = null;
+
+	$addSong = false;
+	$addAlbum = false;
+	$addArtist = false;
+	$addYear = false;
+
+	$songError = false;
+	$albumError = false;
+	$artistError = false;
+
+	if ( isset($_POST["add"]) ) {
+		
+		if ( isset($_POST["song"]) ) {
+			$song = $_POST["song"];
+			if (strlen($song) > 0 ) {
+				$addSong = true;
+
+				// $newMusic = [
+				// 	"song" => $song;
+				// ];
+
+				//Create Song
+
+				//Save Song
+
+
+			} else {
+				$songError = "Please add song title.";
+			}
+		}
+
+		if ( isset($_POST["album"]) ) {
+			$album = $_POST["album"];
+			if (strlen($album) > 0 ) {
+				$addAlbum = true;
+			} else {
+				$albumError = "Please add album title.";
+			}
+		}
+
+		if ( isset($_POST["artist"]) ) {
+			$artist = $_POST["artist"];
+			if (strlen($artist) > 0 ) {
+				$addArtist = true;
+			} else {
+				$artistError = "Please add artist or band name.";
+			}
+		}
+
+		//Not sure how to get it to pick a checked genre???
+		if ( isset($_POST["genre"]) ) {
+			$genre = $_POST["genre"];
+			
+		}
+
+		if ( isset($_POST["year"]) ) {
+			$year = $_POST["year"];
+			if ($year > 1969 && $year < 2023) {
+				$addYear = true;
+			}
+		}
+
+		if ( isset($_POST["picture"]) ) {
+			$picture = $_POST["picture"];
+			
+		}
+
+		if ($addSong && $addAlbum && $addArtist && $addYear) {
+			$music = [
+				$song => $_POST["song"],
+				$album => $_POST["album"],
+				$artist => $_POST["artist"],
+				$genre => $_POST["genre"],
+				$year => $_POST["year"],
+				$picture => $_POST["picture"],
+			];
+
+			var_dump($music);
+		} else {
+			echo "MISSING SOMETHING";
+		}
 	}
 
 ?> 
@@ -14,55 +99,64 @@
 		<form method="POST">
 			<field>
 				<label>Song</label>
-				<input type="text" name="Song" required="required">
+				<input type="text" name="song" value='<?=$song?>'>
+				<?php if ($songError) { ?>
+					<p class="error"><?=$songError?></p>
+				<?php } ?>
 			</field>
 
 			<field>
 				<label>Album</label>
-				<input type="text" name="Album" required="required">
+				<input type="text" name="album" value='<?=$album?>'>
+				<?php if ($songError) { ?>
+					<p class="error"><?=$albumError?></p>
+				<?php } ?>
 			</field>
 
 			<field>
 				<label>Artist</label>
-				<input type="text" name="Artist" required="required">
+				<input type="text" name="artist" value='<?=$artist?>'>
+				<?php if ($songError) { ?>
+					<p class="error"><?=$artistError?></p>
+				<?php } ?>
 			</field>
 
 			<field>
 				<p>Please select the Genre for this song</p>
 				<div class="radio">
-					<input type="radio" id="Rock" name="Genre" value="Rock">
-					<label for="Rock">Rock</label>
+					<input type="radio" id="Rock" name="genre" value="Rock">
+					<label for="rock">Rock</label>
 				</div>
 				<div class="radio">
-					<input type="radio" id="Hip-Hop" name="Genre" value="Hip-Hop">
-					<label for="Hip-Hop">Hip-Hop</label>
+					<input type="radio" id="Hip-Hop" name="genre" value="Hip-Hop">
+					<label for="hip-hop">Hip-Hop</label>
 				</div>
 				<div class="radio">
-					<input type="radio" id="EDM" name="Genre" value="EDM">
-					<label for="EDM">EDM</label>
+					<input type="radio" id="EDM" name="genre" value="EDM">
+					<label for="edm">EDM</label>
 				</div>
 				<div class="radio">
-					<input type="radio" id="Latin" name="Genre" value="Latin">
-					<label for="Latin">Latin</label>
+					<input type="radio" id="Latin" name="genre" value="Latin">
+					<label for="latin">Latin</label>
 				</div>
 				<div class="radio">
-					<input type="radio" id="Pop" name="Genre" value="Pop">
-					<label for="Pop">Pop</label>
+					<input type="radio" id="Pop" name="genre" value="Pop">
+					<label for="pop">Pop</label>
 				</div>
 				<div class="radio">
-					<input type="radio" id="Other" name="Genre" value="Other">
-					<label for="Other">Other</label>
+					<input type="radio" id="Other" name="genre" value="Other" checked>
+					<label for="other">Other</label>
 				</div>
 			</field>
 
 			<field>
 				<label>Year of Song/Album</label>
-				<input type="number" min="1970" max="2022" name="Year" required="required">
+				<input type="number" min="1970" max="2022" name="year" value='<?=$year?>'>
 			</field>
 
 			<field>
 				<label>Album Cover</label>
-				<input type="file" accept="image/*" name="picture">
+				<input type="file" accept="image/*" name="picture" required="required">
 			</field>
 
 			<button type="submit" name="add">Add Song</button>
