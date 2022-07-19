432f759956a157a299471dc96a71e7b9bee8cc87
Steve DeAnda <stevedeanda5@gmail.com>
2022-07-19 16:38:22 -0500

    Add made the data part work
    Took me a while because I couldn’t figure what the issue was and it was my data. Always validate the json data even if it is a little. I had an extra comma.

--- router-site/index.php
+++ router-site/index.php
@@ -1,11 +1,26 @@
 
 <?php include('functions.php'); ?>
 
+<?php
+	$page = currentPage();
+
+	$dataPath = "data/pages/$page.json";
+
+	//echo $dataPath;
+
+	$json = file_get_contents($dataPath);
+	$pageData = json_decode($json, true);
+
+	//echo $pageData['title'];
+?>
+
+
+
 
 <header>
 	<p>?<?=queryString();?></p>
-	<h1><?=$page["title"]?></h1>
-	<p><?=$page["title"]?></p>
+	<h1><?=$pageData["title"]?></h1>
+	<p><?=$pageData["intro"]?></p>
 
 	<?php include('partials/site-menu.php') ?>
 </header>
@@ -13,7 +28,19 @@
 <?php getPageTemplate(currentPage()); ?>
 
 <section>
-	<h2>Section Heading</h2>
+
+	<?php
+
+		$sections = $pageData['sections'];
+		foreach ($sections as $section) { 
+
+		$heading = $section['heading'];?>
+
+			<h2><?=$heading?></h2>
+
+	<?php } ?>
+	
+
 </section>
 
 
