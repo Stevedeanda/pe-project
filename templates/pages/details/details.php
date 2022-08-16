e0ecbfb3bf7b321c7f963039fc2db33707719328
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-15 23:50:07 -0500

    Added detail page

---
+++ templates/pages/details/details.php
@@ -0,0 +1,8 @@
+<?php
+
+//user goes to project page
+//then they click on the project, request the project detail page
+
+//load the project detail page
+//get the project data
+//render the project data
