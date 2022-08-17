e4ee837d3201e74ed0ffe3c62c1077eb458e2c85
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-17 01:37:23 -0500

    Made forms look nicer and cleaner

---
+++ templates/pages/forms/calculating-berries.php
@@ -0,0 +1,48 @@
+<?php
+
+	$guests = 0;
+	$berries = 0;
+	$total = 0;
+
+	if ( isset($_POST["submitted"])) {
+ 	
+	 	if ( isset($_POST["guests"])) {
+	 		$guests = $_POST["guests"];
+	 	}
+	 	if ( isset($_POST["berries"])) {
+	 		if ( $_POST["berries"] >= 0) {
+	 			$berries = $_POST["berries"];
+	 		}
+	 	}
+
+	 	$total = floatval($guests) * floatval($berries);
+ 	}
+
+?>
+
+<header>
+	<a href="?page=forms">Back to form list</a>
+</header>
+
+
+<form method="POST">
+
+	<h1 class="medium-voice">Calculating Berries</h1>
+
+	<p>Let's figure out how many berries we need</p>
+
+	<div class="field">
+		<label>Number of guests</label>
+		<input type="number" name="guests" min="0" value="<?=$guests?>">
+	</div>
+
+	<div class="field">
+		<label>Berries per guest</label>
+		<input type="number" name="berries" min="0" value="<?=$berries?>">
+	</div>
+
+	<button type="submit" name="submitted">Push me</button>
+
+	<p>We will need <span><?=$berries?></span> berries per guest. Since there is <span><?=$guests?></span> guests, we will need a total of <span><?=$total?></span> berries.</p>
+
+</form>
---
+++ templates/pages/forms/drinking-age.php
@@ -0,0 +1,46 @@
+<?php
+	// Initialize default variables
+	$currentYear = 2022;
+	$yearBorn = 2008;
+	$message = null;
+	$age = $currentYear - $yearBorn;
+	
+
+	// Deal with user input
+	if ( isset($_POST["submitted"])) {
+		if (isset($_POST["born"])) {
+			$yearBorn = $_POST["born"];
+			$age = $currentYear - floatval($yearBorn);	
+		}
+	} 
+
+	// Do any calculations 
+	if ($age >= 21) {
+		$message = "You <span>$age</span> years old, you can drink buddy!"; 
+	} else {
+		$message = "You are <span>$age</span> years old, not 21, no alcohol for you kiddo.";
+	}
+?>
+	
+<header>
+	<a href="?page=forms">Back to form list</a>
+</header>
+
+<form method="POST">
+
+	<h1 class="medium-voice">Drinking calculator</h1>
+	
+	<p>Are you of drinking age?</p>
+
+	<div class="field">
+	 	<label>What year were you born?</label>
+	 	<input type="number" name="born" max="2022" min="1900" required="required" value="<?=$yearBorn?>">
+	</div>
+
+	<button type="submit" name="submitted">Let's find out</button>
+
+	<!-- Output -->
+
+	<p><?=$message?></p>
+
+</form>
