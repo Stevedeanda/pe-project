8238d4584df2a455da3f35d43e63442d2f9eb222
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-17 04:15:09 -0500

    Deleted all these files since not needed

--- templates/pages/details/details.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//user goes to project page
-//then they click on the project, request the project detail page
-
-//load the project detail page
-//get the project data
-//render the project data
