63bb39e151ac30003150e29d0f359e96891f73de
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-17 01:34:06 -0500

    Added a if statement and else if to index

--- index.php
+++ index.php
@@ -1,62 +1,39 @@
 
 <?php include('header.php'); ?>
 
-			<p class="query">?<?=queryString();?></p>
+<p class="query">?<?=queryString();?></p>
 
-			<?php
+<?php 
+if ($_GET["page"] == "forms") {
+	if ($json = file_get_contents("templates/pages/forms/forms.json") ) {
+		$formsData = json_decode($json, true);
+		foreach ($formsData as $form) {
+			if ($form['id'] == $_GET['id']) {
+				$theform = $form;
+				$module = $theform['module'];
+			}
+		} ?>
 
-			// if ($page == "home") {
-			// 	include("templates/pages/home/home.php");
-			// }
+		<section>
+			<inner-column>
+				<?php include ("templates/pages/forms/$module.php"); ?>
+			</inner-column>
+		</section>
 
-			// if ($page == "about") {
-			// 	include("templates/pages/about/about.php");
-			// }
-
-			// if ($page == "projects") {
-			// 	include("templates/pages/projects/projects.php");
-			// }
-
-			// if ($page == "resume") {
-			// 	include("templates/pages/resume/resume.php");
-			// }
-
-			// if ($page == "contact") {
-			// }
-
-			// if ($page == "forms") {
-			// 	include("templates/pages/forms/forms.php");
-			// }
-
-			//include("templates/pages/$page/$page.php");
-
-			// $json = file_get_contents("templates/pages/forms/forms.json");
-			// $formNames = json_decode($json, true);
-			// $forms = $formNames["forms"];
-
-			// foreach ($forms as $form) {
-			// 	if ($page == addDashes($form)) {
-			// 		include("templates/pages/forms/exercises/" . addDashes($form). ".php");
-			// 	}
-			// }
-
-			?>
-
-<?php 	
-//Use a if statement to get detail pages.
-if ($_GET["page"] == "details") {
+	<?php } 
+} else if ($_GET["page"] == "project") {
 	//Get the detail data
 	if ($json = file_get_contents("data/projects.json") ) {
 		$detailData = json_decode($json, true);
 		foreach ($detailData as $project) {
 			if ($project['id'] == $_GET['id']) {
 				$detail = $project;
-				$m = $detail['module'];
+				$module = $detail['module'];
 			}
 		}
 		//Render the detail data
 		//echo $detail['name'];
-		include ( "templates/modules/$m/$m.php");
+		include ( "templates/modules/$module/$module.php");
 	}  
 } else {
 	// get the page data
@@ -68,9 +45,7 @@ if ($_GET["page"] == "details") {
 		$module = $section['module'];	// Get correct module template based on name
 		include ( "templates/modules/$module/$module.php");
 	} 
-}
-
-?>
+}	?>
 
 <?php include ('footer.php'); ?>
 
