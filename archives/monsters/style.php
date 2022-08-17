e789b8bd4fbe97b3fdda3b1622e98ac3edc471cb
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-17 01:33:24 -0500

    Added files to archive folder

---
+++ archives/monsters/style.php
@@ -0,0 +1,122 @@
+
+
+
+
+<style>
+
+	.body {
+		background-color: hsl(83, 81%, 81%);
+		/*border: orange solid 2px;*/
+		padding: 10px;
+		font-family: sans serif;
+	}
+
+	.welcome {
+		display: flex;
+		flex-direction: column;
+		/*border: 2px blue solid;*/
+		max-width: 700px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 10px;
+		background-color: #fba942;
+	}
+
+	.menu {
+		/*border: 2px solid gray;*/
+	}
+
+	.menu-list {
+		/*border: 2px solid;*/
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+
+	.menu-list a {
+		color: black;
+	}
+
+	.company {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.company-name {
+		font-size: 28px;
+
+	}
+
+	.inner-column {
+		display: flex;
+		flex-direction: column;
+		/*border: 2px blue solid;*/
+		max-width: 700px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 10px;
+
+	}
+
+	.list {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		/*border: 2px solid green;*/
+		padding: 10px;
+	}
+	
+	monster-card {
+		display: block;
+		border: 1px solid black;
+		border-radius: 10px;
+		padding: 10px;
+		margin-bottom: 10px;
+		background-color: #fef4dd;
+	}
+
+	
+	.name {
+		font-size: 20px;
+		padding: 10px 0;
+	}
+
+	.message {
+		font-size: 14px;
+		padding-bottom: 10px;
+	}
+
+	.status {
+		display: inline-block;
+		padding: 2px 5px;
+		border: black 2px solid;
+		border-radius: 5px;
+		color: black;
+	}
+
+	.status:hover {
+		background-color: black;
+		color: white;
+	}
+
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
