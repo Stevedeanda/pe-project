1f3a191e23692334b75898b104f9929c90670812
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-18 19:42:31 -0500

    Added a way to add a class to the module

--- templates/modules/generic-text/generic-text.php
+++ templates/modules/generic-text/generic-text.php
@@ -2,11 +2,12 @@
 
 $section['heading'] = $section['heading'] ?? "This is where the heading goes";
 $section['content'] = $section['content'] ?? "This is where the content goes in a paragraph element";
+$section['class'] = $section['class'] ?? "";
 
 ?>
 
 
-<generic-text>
+<generic-text class="<?=$section['class']?>">
 	<h2 class="loud-voice"><?=$section['heading']?></h2>
 	<p><?=$section['content']?></p>
 </generic-text>
