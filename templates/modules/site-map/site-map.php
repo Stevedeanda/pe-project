37e9e73df555bfe9045db0040c161a8eb88f504b
Steve DeAnda <stevedeanda5@gmail.com>
2022-08-18 19:41:17 -0500

    Added a site map to the footer

---
+++ templates/modules/site-map/site-map.php
@@ -0,0 +1,10 @@
+	<site-map>
+		<nav class="site-menu">					
+			<a href="?page=home">Home</a>
+			<a href="?page=about">About</a>
+			<a href="?page=projects">Projects</a>
+			<a href="?page=resume">Resume</a>
+			<a href="?page=contact">Contact</a>
+			<a href="?page=style-guide">Style Guide</a>
+		</nav>
+	</site-map>
