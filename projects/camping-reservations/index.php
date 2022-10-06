18d8b25309231c7085ef447c46d56b13ba752c2b
Steve DeAnda <stevedeanda5@gmail.com>
2022-10-06 11:36:28 -0500

    Added some code to JS campsite app

---
+++ projects/camping-reservations/index.php
@@ -0,0 +1,50 @@
+
+
+<!doctype html>
+
+
+<html lang='en'>
+
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title>Camping Time</title>
+		<meta name='description' content='Steve DeAnda website'>
+		<meta property='og:image' content='Welcome to my website!'>
+
+		<link rel='stylesheet' href='style/site.css'>
+	</head>
+
+
+	<body>
+		<header>
+			
+		</header>
+
+		<main>
+
+			<h1>My app for booking campsites</h1>
+
+			<!-- Create a search box that can show a list of state parks for you to pick from  -->
+			<!-- A list of parks should appear as you type in the search bar -->
+			<!-- When you click on a park from search bar, go to that state park -->
+
+			<form>
+			  <div>
+			    <input type="search" name="parks" value="" placeholder="Search for park..."/>
+			    <button>Search</button>
+			  </div>
+			</form>
+
+			
+		</main>
+
+		<script>
+
+
+		</script>
+
+	</body>
+
+</html>
