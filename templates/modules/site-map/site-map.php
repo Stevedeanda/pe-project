5885e22c02c388242473ffaf16ae745c1c11af01
Steve DeAnda <stevedeanda5@gmail.com>
2022-10-25 13:19:47 -0500

    Added goal page and rearrange the footer

--- templates/modules/site-map/site-map.php
+++ templates/modules/site-map/site-map.php
@@ -5,7 +5,7 @@
 		<a class="about" href="?page=about">About</a>
 		<a class="projects" href="?page=projects">Projects</a>
 		<a class="resume" href="?page=resume">Resume</a>
-		<a href="?page=style-guide">Style&nbspGuide</a>
-		<a href="?page=goals">Goals</a>
+		<a class="goal" href="?page=goals">Goals</a>
+		<a class="guide" href="?page=style-guide">Style&nbspGuide</a>
 	</nav>
 </site-map>
