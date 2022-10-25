ac0df4812ee6d1871460021bc8985d5ce88b94bd
Steve DeAnda <stevedeanda5@gmail.com>
2022-10-25 18:05:08 -0500

    Changed the module to have the clasess be in the json

--- templates/modules/summary-text/summary-text.php
+++ templates/modules/summary-text/summary-text.php
@@ -3,11 +3,15 @@
 $section['title'] = $section['title'] ?? "Name of the person this resume belongs to";
 $section['job'] = $section['job'] ?? "Their job title goes here";
 $section['summary'] = $section['summary'] ?? "This is a small summary of what they want the hiring person to see about them, highlighting things to stand out.";
+$section['class'] = $section['class'] ?? "";
+$section['voice-one'] = $section['voice-one'] ?? "";
+$section['voice-two'] = $section['voice-two'] ?? "";
+$section['voice-three'] = $section['voice-three'] ?? "";
 
 ?>
-<summary-text>
-	<h1 class="exciting-voice"><?=$section['title']?></h1>
-	<h2 class="attention-voice"><?=$section['job']?></h2>
-	<p class="calm-voice"><?=$section['summary']?></p>
+<summary-text class="<?=$section['class']?>">
+	<h1 class="<?=$section['voice-one']?>"><?=$section['title']?></h1>
+	<h2 class="<?=$section['voice-two']?>"><?=$section['job']?></h2>
+	<p class="<?=$section['voice-three']?>"><?=$section['summary']?></p>
 </summary-text>
 
