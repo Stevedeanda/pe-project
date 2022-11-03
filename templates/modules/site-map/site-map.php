34c569aaa9b3af326ceea3ab0dcb7b5cb80735aa
Steve DeAnda <stevedeanda5@gmail.com>
2022-11-03 15:23:54 -0500

    Adjusted the footer and added social media links

--- templates/modules/site-map/site-map.php
+++ templates/modules/site-map/site-map.php
@@ -7,5 +7,8 @@
 		<a class="resume" href="?page=resume">Resume</a>
 		<a class="goal" href="?page=goals">Goals</a>
 		<a class="guide" href="?page=style-guide">Style&nbspGuide</a>
+		<a class="github" target="github" href="https://github.com/Stevedeanda">Github</a>
+		<a class="linkedin" target="linkedin" href="https://www.linkedin.com/in/stevedeanda">Linkedin</a>
+
 	</nav>
 </site-map>
