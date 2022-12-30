2a5551cf9d018e3874223db2a53d27f72f80e1bf
Steve DeAnda <stevedeanda5@gmail.com>
2022-12-30 16:35:00 -0600

    Organized my archieved websites

---
+++ my-websites/mark-I/index.php
@@ -0,0 +1,20 @@
+
+<!doctype html>
+
+<html lang="en">
+
+	<head>	
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Steve DeAnda</title>
+		
+		<meta name="description" content="I'm a web developer in the making.">
+		<meta property="og:image" content="https://peprojects.dev/alpha-4/steve/meta-image-personal-website.png">
+		<meta property="og:title" content="Hi, that's me, so far so good.">
+
+		<link rel="stylesheet" href="main-index.css">
+		
+	</head>
+
+	<body>
---
+++ my-websites/mark-III/templates/modules/site-map/site-map.php
@@ -0,0 +1,15 @@
+<!-- Module -->
+<site-map>
+	<nav class="site-menu">					
+		<a class="home" href="?page=home">Home</a>
+		<a class="about" href="?page=about">About</a>
+		<a class="projects" href="?page=projects">Projects</a>
+		<a class="resume" href="?page=resume">Resume</a>
+		<a class="goal" href="?page=goals">Goals</a>
+		<a class="guide" href="?page=style-guide">Style&nbspGuide</a>
+		<a class="github" target="github" href="https://github.com/Stevedeanda">Github</a>
+		<a class="linkedin" target="linkedin" href="https://www.linkedin.com/in/stevedeanda">Linkedin</a>
+		<!-- <a class="goal" href="?page=contact">Contact</a> -->
+
+	</nav>
+</site-map>
