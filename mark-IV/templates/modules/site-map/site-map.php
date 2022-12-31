2d82076a139e3ba58ea8b9faf2cbec103452fd37
Steve DeAnda <stevedeanda5@gmail.com>
2022-12-31 08:59:04 -0600

    Fixed typography and footer for homepage

--- mark-IV/templates/modules/site-map/site-map.php
+++ mark-IV/templates/modules/site-map/site-map.php
@@ -2,18 +2,18 @@
 <site-map>
 	<nav class="site-menu">					
 		<left-menu>
-			<a class="home" href="?page=home">Home</a>
-			<a class="about" href="?page=about">About</a>
-			<a class="projects" href="?page=projects">Projects</a>
+			<a class="home regular-voice" href="?page=home">Home</a>
+			<a class="about regular-voice" href="?page=about">About</a>
+			<a class="projects regular-voice" href="?page=projects">Projects</a>
 		</left-menu>
 		<middle-menu>
-			<a class="resume" href="?page=resume">Resume</a>
-			<a class="goal" href="?page=goals">Goals</a>
-			<a class="guide" href="?page=style-guide">Style&nbspGuide</a>
+			<a class="resume regular-voice" href="?page=resume">Resume</a>
+			<a class="goal regular-voice" href="?page=goals">Goals</a>
+			<a class="guide regular-voice" href="?page=style-guide">Style&nbspGuide</a>
 		</middle-menu>
 		<right-menu>
-			<a class="github" target="github" href="https://github.com/Stevedeanda">Github</a>
-			<a class="linkedin" target="linkedin" href="https://www.linkedin.com/in/stevedeanda">Linkedin</a>
+			<a class="github regular-voice" target="github" href="https://github.com/Stevedeanda">Github</a>
+			<a class="linkedin regular-voice" target="linkedin" href="https://www.linkedin.com/in/stevedeanda">Linkedin</a>
 			<!-- <a class="goal" href="?page=contact">Contact</a> -->
 		</right-menu>
 
