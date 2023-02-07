b9c4f5ea87ea6d94d7d69bd5d55569dc1a485e92
Steve DeAnda <stevedeanda5@gmail.com>
2023-02-07 08:42:41 -0600

    Added modules to figma challenge

---
+++ projects/figma-to-code/templates/modules/nav-four/nav-four.php
@@ -0,0 +1,17 @@
+
+
+
+<section class="<?=$section['class']?>">
+	<inner-column>
+
+		<main-logo>
+			<img src="<?=$section['main-logo']?>">
+		</main-logo>
+
+		<hamburger-icon>
+			<img src="<?=$section['hamburger']?>">
+		</hamburger-icon>
+
+	</inner-column>	
+</section>
+
