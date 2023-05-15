7159335af32262cdbc4c0de64c3997a46e596ec5
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 13:13:03 -0500

    Deleted the frameborder from the video iframe

--- betsy-steve-wedding/templates/modules/resort-video/resort-video.php
+++ betsy-steve-wedding/templates/modules/resort-video/resort-video.php
@@ -9,7 +9,6 @@ $section['class'] = $section['class'] ?? "";
 	<iframe
       src="<?=$section['src']?>"
       title="Riu Palace Costa Mujeres video"
-      frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen
    ></iframe>
