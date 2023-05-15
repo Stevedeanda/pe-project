5c1b14e69d5a494a07ff65863e05461b3bd97b66
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 14:05:16 -0500

    Added font to the buttons

--- betsy-steve-wedding/templates/modules/link-button/link-button.php
+++ betsy-steve-wedding/templates/modules/link-button/link-button.php
@@ -6,6 +6,6 @@ $section['class'] = $section['class'] ?? "";
 $section['target'] = $section['target'] ?? "";
 
 ?>
-<link-button class="<?=$section['class']?>">
+<link-button class="<?=$section['class']?> <?=$section['voice']?>">
 	<a href="<?=$section['href']?>" target="<?=$section['target']?>"><?=$section['content']?></a>
 </link-button>
