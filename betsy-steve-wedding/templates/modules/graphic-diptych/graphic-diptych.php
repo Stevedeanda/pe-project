9b2035021ac740104fb100213c1aac4894e5a9e2
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 15:00:41 -0500

    Added alt attribute to images

--- betsy-steve-wedding/templates/modules/graphic-diptych/graphic-diptych.php
+++ betsy-steve-wedding/templates/modules/graphic-diptych/graphic-diptych.php
@@ -7,6 +7,6 @@ $section['class'] = $section['class'] ?? "";
 
 <graphic-diptych class="<?=$section['class']?>">
 	<picture>
-		<img src="<?=$section['url']?>" alt="">
+		<img src="<?=$section['url']?>" alt="<?=$section['alt']?>">
 	</picture>
 </graphic-diptych>
--- betsy-steve-wedding/templates/modules/two-pictures/two-pictures.php
+++ betsy-steve-wedding/templates/modules/two-pictures/two-pictures.php
@@ -8,9 +8,9 @@ $section['class'] = $section['class'] ?? "";
 
 <two-pictures class="<?=$section['class']?>">
 	<picture class="<?=$section['class-two']?>">
-		<img src="<?=$section['urlone']?>" alt="">
+		<img src="<?=$section['urlone']?>" alt="<?=$section['altone']?>">
 	</picture>
 	<picture class="<?=$section['class-three']?>">
-		<img src="<?=$section['urltwo']?>" alt="">
+		<img src="<?=$section['urltwo']?>" alt="<?=$section['alttwo']?>">
 	</picture>
 </two-pictures>
