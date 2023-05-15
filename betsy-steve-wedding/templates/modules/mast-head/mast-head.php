66afab259a30ac3f197c46627515cf696e96d5d9
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 13:41:36 -0500

    Adjusted the underline hover on the nav

--- betsy-steve-wedding/templates/modules/mast-head/mast-head.php
+++ betsy-steve-wedding/templates/modules/mast-head/mast-head.php
@@ -4,6 +4,5 @@
 		<a class="home nav-voice" href="?page=home">Home</a>
 		<a class="resort nav-voice" href="?page=resort">Resort</a>
 		<a class="rsvp nav-voice" href="?page=rsvp">RSVP</a>
-		<!-- <a class="resume" href="?page=resume">Resume</a> -->
 	</nav>
 </mast-head>
