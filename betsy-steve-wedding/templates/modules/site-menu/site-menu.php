b628591f7dc30878b342493ad766732d0bdff8f7
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 12:32:02 -0500

    Deleted a trailing slash and added alt attribute to img tag

--- betsy-steve-wedding/templates/modules/site-menu/site-menu.php
+++ betsy-steve-wedding/templates/modules/site-menu/site-menu.php
@@ -6,7 +6,7 @@
 	<?php include ('templates/modules/mast-head/mast-head.php'); ?> <!-- modules goes here-->
 
 		<picture>
-			<img src="images/betsy-steve-checkershirts.jpeg" />
+			<img src="images/betsy-steve-checkershirts.jpeg" alt="Couple at the top of mountain wearing black and red flannel  shirts.">
 		</picture>
 	</inner-column>	
 </header>
