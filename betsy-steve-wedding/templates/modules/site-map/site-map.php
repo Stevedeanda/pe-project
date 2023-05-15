5584602d1d034a1e6ca81a45840243af6891aa89
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 17:01:13 -0500

    Made changes to styles and fonts

--- betsy-steve-wedding/templates/modules/site-map/site-map.php
+++ betsy-steve-wedding/templates/modules/site-map/site-map.php
@@ -1,8 +1,8 @@
 	<site-map>
 		<nav class="site-menu">					
-			<a class="home small-voice" href="?page=home">Home</a>
-			<a class="resort small-voice" href="?page=resort">Resort</a>
-			<a class="rsvp small-voice" href="?page=rsvp">RSVP</a>
+			<a class="small-voice" href="?page=home">Home</a>
+			<a class="small-voice" href="?page=resort">Resort</a>
+			<a class="small-voice" href="?page=rsvp">RSVP</a>
 		</nav>
 		<p class="small-voice">Website Created by <a href="https://stevedeanda.dev/" target="_steve">Steve</a></p>
 	</site-map>
