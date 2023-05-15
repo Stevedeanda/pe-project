55f087115d753d6057560eeaa9d16a7e4dd3f489
Steve DeAnda <stevedeanda5@gmail.com>
2023-05-15 12:29:07 -0500

    Changed the h1 to an h2 in the summary text module

--- betsy-steve-wedding/templates/modules/summary-text/summary-text.php
+++ betsy-steve-wedding/templates/modules/summary-text/summary-text.php
@@ -9,7 +9,7 @@ $section['class-one'] = $section['class-one'] ?? "";
 
 ?>
 <summary-text class="<?=$section['class']?>">
-	<h1 class="<?=$section['class-one']?>"><?=$section['title']?></h1>
+	<h2 class="<?=$section['class-one']?>"><?=$section['title']?></h2>
 	<p class="<?=$section['class-two']?>"><?=$section['date']?></p>
 	<p class="<?=$section['class-two']?>"><?=$section['time']?></p>
 	<p class="<?=$section['class-two']?>"><?=$section['place']?></p>
