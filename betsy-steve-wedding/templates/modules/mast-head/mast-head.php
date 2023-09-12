6e29c295ba9d8c0b54bf50828879fbf862a9ffd7
Steve DeAnda <stevedeanda5@gmail.com>
2023-09-12 12:03:15 -0500

    test 3 and done

--- betsy-steve-wedding/templates/modules/mast-head/mast-head.php
+++ betsy-steve-wedding/templates/modules/mast-head/mast-head.php
@@ -2,7 +2,7 @@
 <mast-head>
 	<nav class="site-menu">					
 		<a class="nav-voice" href="?page=home">Home</a>
-		<a class="nav-voice" href="?page=resort">Resortss</a>
+		<a class="nav-voice" href="?page=resort">Resort</a>
 		<a class="nav-voice" href="?page=rsvp">RSVP</a>
 	</nav>
 </mast-head>
